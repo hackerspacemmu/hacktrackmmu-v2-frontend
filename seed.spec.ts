3c7284b2f0694c2bd46cde9e1d4c7053d6285ab7
Tharshen2124 <tharshensurianbalan@gmail.com>
2026-09-06 19:50:20 +0800

    chore: generate test spec for test generation

--- seed.spec.ts
+++ seed.spec.ts
@@ -2,6 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test.describe('Test group', () => {
   test('seed', async ({ page }) => {
+    await page.goto('http://localhost:8000/login');
     // generate code here.
   });
 });
